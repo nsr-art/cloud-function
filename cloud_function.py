74e9c57546a8a923165e6643258ee22e9d7c39a5
Natthapon Sriraksungnoen <natthapon.srir@bumail.net>
2020-11-20 20:01:11 +0700

    update 20/11/2020

--- cloud_function.py
+++ cloud_function.py
@@ -1,34 +1,25 @@
+from datetime import datetime
+import time
 import base64
+import json
 import sqlalchemy
-
-connection_name = "pivotal-sprite-285504:asia-southeast1:smarthome-seniorproject"
-table_name = ""
-#table_field = ""
-#table_field_value = ""
+import pytz
+connection_name = "driven-bison-295809:us-central1:senior-project"
 db_name = "smart_home"
-db_user = "NSR_ADMIN"
+db_user = "nsr-admin"
 db_password = "natthapon024299"
-
-# If your database is MySQL, uncomment the following two lines:
 driver_name = 'mysql+pymysql'
 query_string = dict({"unix_socket": "/cloudsql/{}".format(connection_name)})
-
 def hello_pubsub(event, context):
-    """Triggered from a message on a Cloud Pub/Sub topic.
-    Args:
-         event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the event.
-    """
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
-    date = pubsub_message[2:12]
-    time = pubsub_message[13:21]
-    temp = pubsub_message[31:35]
-    humi = pubsub_message[37:41]
-    stat = pubsub_message[43]
-
-    #stmt = INSERT(DHT11_Sensor).VALUES(Date=date,Time=time,Temperature=temp,Humidity=humi,Status=stat,Voltage=5)
-    #stmt = sqlalchemy.text('INSERT INTO DHT11_Sensor (Date, Time, Temperature, Humidity, Status, Voltage) VALUES (22-0-2020,14.27,23.6,50.8,1,5)')
-    sql = "INSERT INTO DHT11_Sensor (Date, Time, Temperature, Humidity, Status, Voltage) VALUES (%s,%s,%s,%s,%s,%s)"
+    x = json.loads(pubsub_message)
+    deviceid = x['device_id']
+    roomid = x['roomid']
+    dt_object = datetime.fromtimestamp(x['Date'], pytz.timezone('Asia/Bangkok'))
+    time_string = str(dt_object.strftime("%Y-%m-%dT%H:%M:%SZ"))
+    temp = x['Temp']
+    humi = x['Humi']
+    sql = "INSERT INTO dht11(device_id, room_id, datetime, temperature, humidity) VALUES (%s,%s,%s,%s,%s)"
     db = sqlalchemy.create_engine(
       sqlalchemy.engine.url.URL(
         drivername=driver_name,
@@ -44,7 +35,7 @@ def hello_pubsub(event, context):
     )
     try:
         with db.connect() as conn:
-            conn.execute(sql, (date,time,temp,humi,stat,5))
+            conn.execute(sql, (deviceid, roomid, time_string, temp, humi))
     except Exception as e:
         return 'Error: {}'.format(str(e))
-    print(date, time, temp, humi, stat)
+    print(type(dt_object))
